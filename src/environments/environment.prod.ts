b1211fb0b0810f1c74cc621e7b422debed4fcf77
AhmedEhabWeka <79596162+AhmedEhabWeka@users.noreply.github.com>
2021-08-30 21:56:31 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyDcOd9V9PDIfAvnMgNf5xKufJBNWAeaJjY",
+    authDomain: "test-7bba2.firebaseapp.com",
+    databaseURL: "https://test-7bba2-default-rtdb.firebaseio.com",
+    projectId: "test-7bba2",
+    storageBucket: "test-7bba2.appspot.com",
+    messagingSenderId: "797320647167",
+    appId: "1:797320647167:web:3ba019772fc7698ae517ca",
+    measurementId: "G-9H0K3FD0K7"
+  }
 };
